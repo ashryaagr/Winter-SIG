8eb81e3fb000b7c8fad1d09fbb0e13bdf5eaf889
pratham1002 <54350608+pratham1002@users.noreply.github.com>
2019-12-25 15:39:37 +0530

    Update settings.py

--- election_portal/election_portal/settings.py
+++ election_portal/election_portal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,3 +131,6 @@ STATIC_ROOT=os.path.join(BASE_DIR,'assets')
 
 MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
 MEDIA_URL='/media/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
